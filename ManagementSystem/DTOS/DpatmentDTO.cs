f26ea6be3a0160a4967447cdb7825737b746f3ac
MohamedReda404 <mohamedredaodah89@gmial.com>
2026-01-26 12:53:19 +0200

    Revert "Add project files."
    This reverts commit 2a2644a8280a910e2d34b12b919cb4de18eb1bf9.

--- ManagementSystem/DTOS/DpatmentDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ManagementSystem.DTOS
-{
-    public class DpatmentDTO
-    {
-            public int Id { get; set; }
-            public string Name { get; set; }= string.Empty;
-        public string ManagerName { get; set; }=string.Empty;
-    }
-}
